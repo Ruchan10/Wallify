693dfa54aa1ed613e577e439e648370563936136
Inovaara Developer <ruchan.kayastha@inovaara.com>
2025-09-16 14:04:45 +0545

    Pages Added

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -44,21 +44,21 @@ android {
         versionName = flutterVersionName
     }
 
-    // signingConfigs {
-    //     create("release") {
-    //         storeFile = file(keystoreProperties["storeFile"] ?: "")
-    //         storePassword = keystoreProperties["storePassword"] as String?
-    //         keyAlias = keystoreProperties["keyAlias"] as String?
-    //         keyPassword = keystoreProperties["keyPassword"] as String?
-    //     }
-    // }
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProperties["storeFile"] ?: "")
+            storePassword = keystoreProperties["storePassword"] as String?
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+        }
+    }
 
     buildTypes {
-        // getByName("release") {
-        //     isMinifyEnabled = false
-        //     isShrinkResources = false
-        //     signingConfig = signingConfigs.getByName("release")
-        // }
+        getByName("release") {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            signingConfig = signingConfigs.getByName("release")
+        }
         debug {
             applicationIdSuffix = ".debug"   
             versionNameSuffix = "-debug"     
